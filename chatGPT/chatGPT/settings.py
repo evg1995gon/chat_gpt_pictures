24c96327e946beeaed151144363492e3846d9c8e
Евгений Гончаров <evg1995gon@gmail.com>
2023-03-19 02:38:58 -0700

    deploy

--- chatGPT/chatGPT/settings.py
+++ chatGPT/chatGPT/settings.py
@@ -10,8 +10,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-b%t1gn2&lmtfa_6q*@k*&inmx#4*6v0mmc(fi#a+#k@7pw76p3'
 
+SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
